10fef5be14f30f9ebccb0f82217820f3e76c5f62
yacine zegrour <yacinezegrour@gmail.com>
2021-05-31 16:44:57 +0200

    v3

--- js/script.js
+++ js/script.js
@@ -1,13 +1,31 @@
-console.log("sa marche");
+const musiqueRecuperer = document.querySelectorAll("#musiques > ul> li > button")
 
-const musiqueRecuperer = document.querySelectorAll("#musiques > ul> li")
 
-musiqueRecuperer.forEach((element , i) => {     
+musiqueRecuperer.forEach((button , i) => {     
 
-    element.addEventListener('click', () => {
-    
-        console.log("sa marche");
+    button.addEventListener('click', () => {    
+        const nouvelLi = document.createElement('li');
+        const texte = document.createTextNode(button.getAttribute('data-song'));
+        nouvelLi.append(texte);
+        lesSons.append(nouvelLi);
     })
 })
 
+//console.log(playlist[1])
 
+// const boutonAfficher = document.getElementById("playlist");
+
+// boutonAfficher.addEventListener('click',() => {
+
+//     playlist.forEach((element , i)=>{
+//         console.log(playlist[i])
+
+        
+//         const pText = document.createElement('li');
+//         pText.innerHTML = playlist[i].textContent
+//         lesSons.append(pText)
+       
+
+//     })
+     
+// })
