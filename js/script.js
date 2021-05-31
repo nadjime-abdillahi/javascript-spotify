183a0721ad5fe1af0c70517af8e7a7da2752ddc5
yacine zegrour <yacinezegrour@gmail.com>
2021-05-31 15:12:25 +0200

    v2

--- js/script.js
+++ js/script.js
@@ -1,27 +1,13 @@
 console.log("sa marche");
 
-const musiquerecuperer = document.querySelectorAll("#musiques > ul> li")
+const musiqueRecuperer = document.querySelectorAll("#musiques > ul> li")
 
+musiqueRecuperer.forEach((element , i) => {     
 
-const button1 = document.querySelectorAll("#musiques > ul> li> button")[0]
-const button2 = document.querySelectorAll("#musiques > ul> li> button")[1]
-const button3 = document.querySelectorAll("#musiques > ul> li> button")[2]
-const button4 = document.querySelectorAll("#musiques > ul> li> button")[3]
-
-button1.addEventListener('click', () => {
+    element.addEventListener('click', () => {
     
-    
+        console.log("sa marche");
+    })
 })
 
-button2.addEventListener('click', () => {
-    
-    console.log("sa marche");
-})
-button3.addEventListener('click', () => {
-    
-    console.log("sa marche");
-})
-button4.addEventListener('click', () => {
-    
-    console.log("sa marche");
-})
+
