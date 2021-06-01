635ec36a0d875cf5456fff7c8e68d84887475c87
yacine zegrour <yacinezegrour@gmail.com>
2021-06-01 14:45:07 +0200

    v3

--- js/script.js
+++ js/script.js
@@ -4,10 +4,28 @@ const musiqueRecuperer = document.querySelectorAll("#musiques > ul> li > button"
 musiqueRecuperer.forEach((button , i) => {     
 
     button.addEventListener('click', () => {    
+        // Creation balise li
         const nouvelLi = document.createElement('li');
-        const texte = document.createTextNode(button.getAttribute('data-song'));
-        nouvelLi.append(texte);
+        // Creattion de texte avec en paramettre un get de l'attribut data-song
+        const nomMusique = document.createTextNode(button.getAttribute('data-song'));
+        // Creation d'un bouton supprimer
+        const buttonDelete = document.createElement('button')
+        // Creation du text du boutton
+        buttonDelete.textContent="supprimer";
+        // Ajout de l'event click pour le boutton supprimer
+        buttonDelete.addEventListener('click',()=>{
+        // on supprimer le nom de la musique
+        nouvelLi.remove(nomMusique)
+        // on supprime egalement le boutton supprimer
+        nouvelLi.remove(buttonDelete)
+        })
+        //Attribution a la balise ul le nouvelle Li
         lesSons.append(nouvelLi);
+        //Attribution a la balise li nouveau button
+        nouvelLi.append(buttonDelete)
+        //Attribution a la balise li le nouveau text
+        nouvelLi.append(nomMusique);
+       
     })
 })
 
