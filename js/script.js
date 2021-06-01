908d5dc05af0108a715e50f096a8e67a30168616
yacine zegrour <yacinezegrour@gmail.com>
2021-06-01 14:51:09 +0200

    v4

--- js/script.js
+++ js/script.js
@@ -29,21 +29,3 @@ musiqueRecuperer.forEach((button , i) => {
     })
 })
 
-//console.log(playlist[1])
-
-// const boutonAfficher = document.getElementById("playlist");
-
-// boutonAfficher.addEventListener('click',() => {
-
-//     playlist.forEach((element , i)=>{
-//         console.log(playlist[i])
-
-        
-//         const pText = document.createElement('li');
-//         pText.innerHTML = playlist[i].textContent
-//         lesSons.append(pText)
-       
-
-//     })
-     
-// })
