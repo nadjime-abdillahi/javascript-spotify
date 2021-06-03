890ec05c7b3e2f4ca254ce8171bfe24eb829adc8
yacine zegrour <yacinezegrour@gmail.com>
2021-06-03 14:08:16 +0200

    recherche

--- js/script.js
+++ js/script.js
@@ -2,15 +2,15 @@
         const clienId = '413bb48cf42d45d8918c7f444cf9052f'
         const redirectURI = 'http://localhost:5500/javascript-spotify/'
         let accessToken = null
-        
+        const inputSearch = document.getElementById('search')
         
         
         // Recupere toutes les musiques stocké
-const musiqueRecuperer = document.querySelectorAll("#musiques > ul> li > button")
+        const musiqueRecuperer = document.querySelectorAll("#musiques > ul> li > button")
         // parcours les musiques
-musiqueRecuperer.forEach((button , i) => {     
+        musiqueRecuperer.forEach((button , i) => {     
         // creation de l'event du boutton d'ajout a la playliste
-    button.addEventListener('click', () => {    
+        button.addEventListener('click', () => {    
         // Creation balise li
         const nouvelLi = document.createElement('li');
         // Creattion de texte avec en paramettre un get de l'attribut data-song
@@ -65,10 +65,48 @@ const searchSong = () => {
 		},
 	}
 	console.log(accessToken)
-	fetch("https://api.spotify.com/v1/search?q=muse&type=track", options)
+        fetch(`https://api.spotify.com/v1/search?q=${inputSearch.value}&type=track`, options)
 		.then(response => response.json())
 		.then(data => {
-			console.log(data)
-		})
+			console.log(data);
+                        const li = document.createElement('li');
+                        const button = document.createElement('button');
+                        button.textContent="Ajouter";
+                         const liText = document.createTextNode(`${data.tracks.items[0].name} - ${data.tracks.items[0].artists[0].name}`)
+                          const addbuttons = document.createElement('Ajouter');
+                          const btnText = document.createElement('Add')
+                          li.append(button)
+                          li.append(liText)
+                          musiques.append(li)
+
+                          addbuttons.append(btnText)
+                         
+                          button.addEventListener('click', () => {    
+                                // Creation balise li
+                                const nouvelLi = document.createElement('li');
+                                // Creattion de texte avec en paramettre un get de l'attribut data-song
+                                const nomMusique = document.createTextNode(`${data.tracks.items[0].name} - ${data.tracks.items[0].artists[0].name}`);
+                                // Creation d'un bouton supprimer
+                                const buttonDelete = document.createElement('button')
+                                // Creation du text du boutton
+                                buttonDelete.textContent="supprimer";
+                                // Ajout de l'event click pour le boutton supprimer
+                                buttonDelete.addEventListener('click',()=>{
+                                // on supprimer le nom de la musique
+                                nouvelLi.remove(nomMusique)
+                                // on supprime egalement le boutton supprimer
+                                nouvelLi.remove(buttonDelete)
+                                })
+                                // Attribution a la balise ul le nouvelle Li
+                                lesSons.append(nouvelLi);
+                                // Attribution a la balise li nouveau button
+                                nouvelLi.append(buttonDelete)
+                                // Attribution a la balise li le nouveau text
+                                nouvelLi.append(nomMusique);
+                               
+                            })
+                       
+		
+        })
 }
 bouttonRecherche.addEventListener('click', searchSong)
